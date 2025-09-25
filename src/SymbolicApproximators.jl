adcde4288ecb7051c9178fef4d0f1c266c27aa1b
Michael Myers <michael.myers@wustl.edu>
2025-09-25 17:09:23 -0500

    progress on refactor

--- src/SymbolicApproximators.jl
+++ src/SymbolicApproximators.jl
@@ -12,15 +12,14 @@ export alphabet, breakpoints, cardinality, alphabet_size
 include("words.jl")
 export Word
 
+include("encode.jl")
+export encode
+
 # main algorithms
 include("paa.jl")
 include("sax.jl")
-include("ordinal.jl")
-include("delta.jl")
-export PAA, SAX, OrdinalApproximator, DeltaApproximator
-export discretize, reconstruct, distance
-
-include("windows.jl")
-export StreamingApproximator, update!, get_symbols
+#include("ordinal.jl")
+#include("delta.jl")
+export PAA, SAX #, OrdinalApproximator, DeltaApproximator
 
 end
