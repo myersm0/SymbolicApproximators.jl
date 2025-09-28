654abd215fc23483de4c52cc75637567b1b723fa
Michael Myers <120071679+myersm0@users.noreply.github.com>
2025-09-28 15:44:34 -0500

    Update convergence.jl

--- examples/convergence.jl
+++ examples/convergence.jl
@@ -7,11 +7,11 @@ signal1 = sin.(range(0, 4π, length = 100) |> x -> (x .- mean(x)) ./ std(x))
 signal2 = cos.(range(0, 4π, length = 100) |> x -> (x .- mean(x)) ./ std(x))
 true_distance = euclidean(signal1, signal2)
 
-parameters = [(5,5), (10,10), (25,25), (50,50), (100,100)]
+parameters = [(5, 5), (10, 10), (25, 25), (50, 50), (100, 100)]
 results = map(parameters) do (w, α)
 	sax = SAX(w, α)
 	sax_distance = evaluate(MinDist(), encode(sax, signal1), encode(sax, signal2))
-	bits = w * log2(α)  # Total bits needed to store the SAX representation
+	bits = w * log2(α)  # total bits needed to store the SAX representation
 	error = 100 * (true_distance - sax_distance) / true_distance
 	(bits = bits, error = error)
 end
@@ -22,7 +22,7 @@ y = [r.error for r in results]
 fig = Figure()
 ax = Axis(
 	fig[1,1], 
-	xlabel = "Representation size (bits)", ylabel="Approximation error (%)", 
+	xlabel = "Representation size (bits)", ylabel = "Approximation error (%)", 
 	title = "SAX distance convergence", 
 	xscale = log10
 )
